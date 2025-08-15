72b0c105803c70e5cfac09f115892c4ec7f93adf
Sharath Raj <sharathrj143@gmail.com>
2025-08-16 01:42:08 +0530

    Develop (#5)
    * end of section 2 (#1)
    
    * Set up Angular project with configuration and dependencies
    
    * end of section 2
    
    * Set up Angular project with configuration and dependencies
    
    - Added configuration files: .editorconfig, .gitignore, .postcssrc.json, extensions.json, launch.json, tasks.json, and angular.json.
    - Created package.json with essential scripts and dependencies including Angular, Tailwind CSS, and Vite.
    - Introduced TypeScript configuration files with strict type checking.
    - Added application files: app.config.ts, app.html, app.routes.ts, app.ts, index.html, and main.ts.
    - Included new assets: favicon.ico and user.png.
    - Added SSL certificate files for secure local development.
    
    * Feature/create web (#4)
    
    * end of section 2
    
    * Set up Angular project with configuration and dependencies
    
    - Added configuration files: .editorconfig, .gitignore, .postcssrc.json, extensions.json, launch.json, tasks.json, and angular.json.
    - Created package.json with essential scripts and dependencies including Angular, Tailwind CSS, and Vite.
    - Introduced TypeScript configuration files with strict type checking.
    - Added application files: app.config.ts, app.html, app.routes.ts, app.ts, index.html, and main.ts.
    - Included new assets: favicon.ico and user.png.
    - Added SSL certificate files for secure local development.
    
    * Add CORS support for frontend communication
    
    This commit introduces CORS (Cross-Origin Resource Sharing) support to the application. The `AddCors()` method is added to the service collection, allowing any headers and methods from specified origins (http://localhost:4200 and https://localhost:4200). This change enables the frontend application to communicate with the backend API without being blocked by the browser's same-origin policy.
    
    * Add JWT authentication and user management features
    
    - Updated `API.csproj` to include necessary packages for JWT and EF Core.
    - Created `AccountController` for user registration and login with HMAC password hashing.
    - Introduced `BaseApiController` for common controller functionality.
    - Modified `MembersController` and `WeatherForecastController` to inherit from `BaseApiController`.
    - Added DTOs: `LoginDTO`, `RegisterDTO`, and `UserDto` for data transfer.
    - Updated migration files and `AppUser` class to include `PasswordHash` and `PasswordSalt`.
    - Implemented `ITokenService` interface and `TokenService` class for JWT token generation.
    - Configured authentication and authorization in `Program.cs`.
    - Added `TokenKey` in `appsettings.Development.json` for secure token handling.

---
+++ client/src/app/app.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
+})
+export class App implements OnInit {
+  
+  private http = inject(HttpClient);
+  protected  title = 'Dating App';
+  protected members= signal<any>([]);
+
+  async ngOnInit() {
+    this.members.set(await this.getMembers());
+  }
+
+  async getMembers(){
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    }
+    catch (error) {
+      console.error('Error fetching members:', error);
+      throw error; // Re-throw the error to handle it in the calling context
+    }
+  }
+
+}
